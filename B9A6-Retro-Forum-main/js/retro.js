60a4ba925d6ef55e4a4e84f83c243ef954bb2fb0
coder-utshab <ghoshutshob470@gmail.com>
2026-02-24 19:35:43 +0530

    up

--- B9A6-Retro-Forum-main/js/retro.js
+++ B9A6-Retro-Forum-main/js/retro.js
@@ -14,16 +14,7 @@ const searchPost = (data) => {
    card.classList = "card w-96 bg-base-100 shadow-xl image-full";
 
    card.innerHTML = `
-      <figure>
-         <img src="https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa" />
-      </figure>
-      <div class="card-body">
-         <h2 class="card-title">Shoes!</h2>
-         <p>Example Card</p>
-         <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
-         </div>
-      </div>
+     
    `;
 
    // append card inside banner-section
